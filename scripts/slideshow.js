1f6dd3369fe96c31ef43f975790b3029a3ebc442
Jennifer <jawoowar@googlemail.com>
2026-01-08 16:21:19 +0000

    sorting the rest

---
+++ scripts/slideshow.js
@@ -0,0 +1,21 @@
+// this code was taken from W3Schools due to time constraints - https://www.w3schools.com/howto/howto_js_slideshow.asp
+
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "flex";  
+}
