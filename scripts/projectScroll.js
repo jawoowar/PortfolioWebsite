d20de55220260f0b12ceb0202dc80c8089749387
Jennifer <jawoowar@googlemail.com>
2026-03-06 15:29:06 +0000

    changes to past project

---
+++ scripts/projectScroll.js
@@ -0,0 +1,10 @@
+
+
+function revealProject() {
+    const rowTop = document.querySelector('.rowTop');
+    const mainProject = document.querySelector('.mainProject');
+    const titleLine = document.querySelector('.titleLine');
+    const h3 = document.querySelector('h3');
+    const rowContence = document.querySelector('.rowContence');
+    const arrowScroll = document.querySelector('.arrowScroll');
+}
